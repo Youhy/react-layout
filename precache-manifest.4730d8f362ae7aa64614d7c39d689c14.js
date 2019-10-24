c2c4d530b4326eec7e9043811256728dd3215109
Yauheni Pozdnyakov <youhypozdny@gmail.com>
2019-10-24 10:13:38 +0300

    Updates

---
+++ precache-manifest.4730d8f362ae7aa64614d7c39d689c14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a97428c9ffc2693b8cc208fa2a452cde",
+    "url": "/react-layout/index.html"
+  },
+  {
+    "revision": "eef1905bd6ae57f65822",
+    "url": "/react-layout/static/css/main.84620316.chunk.css"
+  },
+  {
+    "revision": "8b3af57845a5f6e8b541",
+    "url": "/react-layout/static/js/2.8d5c727e.chunk.js"
+  },
+  {
+    "revision": "eef1905bd6ae57f65822",
+    "url": "/react-layout/static/js/main.2c65713f.chunk.js"
+  },
+  {
+    "revision": "da35fc376801a84ad0ae",
+    "url": "/react-layout/static/js/runtime-main.08813ad6.js"
+  }
+]);
